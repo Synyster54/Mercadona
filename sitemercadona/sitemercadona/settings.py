c04b69b872102b72ef0dd4d9befbcf02cbd94d88
MASSON Cyril <massoncyril@MacBook-Pro-de-MASSON.local>
2023-12-01 12:37:03 +0100

    feat : implementation tests

--- sitemercadona/sitemercadona/settings.py
+++ sitemercadona/sitemercadona/settings.py
@@ -91,19 +91,19 @@ WSGI_APPLICATION = 'sitemercadona.wsgi.application'
 #}
 #on utilise une base de données postgresql avec ses données de connexion
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mercadona',
-        'USER': 'postgres',
-        'PASSWORD': '24081990',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
 #DATABASES = {
-    #'default': dj_database_url.parse(config('DATABASE_URL'))
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'mercadona',
+#        'USER': 'postgres',
+#        'PASSWORD': '24081990',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
 #}
+DATABASES = {
+    'default': dj_database_url.parse(config('DATABASE_URL'))
+}
 
 
 # Password validation
